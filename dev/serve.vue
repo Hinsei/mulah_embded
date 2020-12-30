85bc822c1374ade00002d48c730ffef0a8a1b96b
Aiman Asyraf Bin Anuar <imbacakes@gmail.com>
2020-12-30 16:23:13 +0800

    packaging for cdn

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Mulah_test from '@/mulah_test.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Mulah_test
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <mulah_test />
+  </div>
+</template>
